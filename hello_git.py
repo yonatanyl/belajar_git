50cd2027a3a426e4987ba50a98ca99e77fe586b5
yonatanyl <120643853+yonatanyl@users.noreply.github.com>
2025-03-16 11:20:08 +0700

    Menambahkan fungsi Pembagian

--- hello_git.py
+++ hello_git.py
@@ -1,3 +1,9 @@
 print("Hello World and Hello Git!!!")
 
 print("Update Penambahan untuk ceh Pull")
+
+def fungsi_pembagian(a, b) :
+  if b > 0:
+    return a/b
+  else:
+    print("Tidak bisa 0 untuk Pembagi")
